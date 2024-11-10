b5ba0714a419bbab41e91cd14e6fed28742c88b8
duan1-thanhcong <nguyenthanhcongkg1996@gmail.com>
2024-11-10 22:29:04 +0700

    cập nhật đăng ký, đăng nhập

---
+++ views/dangnhap.php
@@ -0,0 +1,31 @@
+<form action="" method="post">
+    <input type="text" name="user" placeholder="Tên tài khoản">
+    <input type="password" name="pass" placeholder="Mật khẩu">
+    <button type="submit" name="dangnhap">Đăng nhập</button>
+
+</form>
+
+<?php
+
+if (isset($_POST["dangnhap"])) {
+    $user = $_POST["user"];
+    $pass = $_POST["pass"];
+
+    foreach ($login as $value) {
+
+        if ($user == $value->user && $pass == $value->pass) {
+            $_SESSION["user"] = $user;
+            echo "Đăng nhập thành công!";
+            include_once "views/da.php";
+        } else {
+            echo "Tài khoản hoặc mật khẩu của bạn sai!";
+        }
+    }
+    session_start();
+    if (isset($_SESSION["user"])) {
+        unset($_SESSION["user"]);
+    }
+    // header("location:dangky.php");
+}
+   
+?>
---
+++ views/dangxuat.php
@@ -0,0 +1,9 @@
+<?php
+    session_start();
+
+    if(isset($_SESSION["user"])) {
+        unset($_SESSION["user"]);
+        // echo "Đăng xuất thành công!";
+    }
+    header("location: dangnhap.php");
+?>
