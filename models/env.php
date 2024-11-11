d93163d144f4d690b7560ec841861833efb6b78e
binhboong795 <binhlqph49920@gmail.com>
2024-11-11 23:30:48 +0700

    sửa file env

--- models/env.php
+++ models/env.php
@@ -1,7 +1,7 @@
 <?php
     const DBNAME = "duan1";
     const DBUSER = "root";
-    const DBPASS = "";
+    const DBPASS = "<điền pass database của riêng mình vào đây";
     const DBHOST = "127.0.0.1";
     const DBCHARSET = "utf8";
 ?>
