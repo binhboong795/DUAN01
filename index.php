82b29cc1b67120df5b3d18bf202043b55da7d931
vietanh2403 <anhtvph49906@gmai.com>
2024-11-11 23:43:18 +0700

    vanh

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php
-require_once 'commons/function.php';
-
-
-
-$act=$_GET['act']??'/';
-match ($act) {
-
-};
+         if(isset($_GET['pg'])) {
+            switch ($_GET['pg']) {
+                case 'product':
+                include_once "controllers/product.php";
+                break;
+                default:
+                        # code...
+                        break;
+            }
+         }else {
+                include_once "views/header.php";
+                include_once "views/home.php";
+                include_once "views/footer.php";
+         }            
+ 
 ?>
