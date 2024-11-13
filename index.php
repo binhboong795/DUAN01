6228d2f713ab291d4b76a7896f82d865ab8a005b
binhboong795 <binhlqph49920@gmail.com>
2024-11-13 09:51:03 +0700

    Merge branch 'main' of https://github.com/binhboong795/DUAN01 into binh

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-<<<<<<< HEAD
+
 require_once 'commons/function.php';
 require_once 'controllers/homeController.php';
 require_once 'models/homeModel.php';
@@ -16,41 +16,6 @@ match ($act) {
     '404' => (new homeController())->error(),
     'chackout' => (new homeController())->chackout(),
 };
-=======
-    
-    // include_once "views/header.php";
-    require_once 'models/User.php';
-    require_once 'controllers/UserController.php';
-    $user = new UserController();
-  
-    $luachon = isset($_GET['act']) ? $_GET['act'] : '/';
+
  
-    switch($luachon) {
-        // case 'product':
-        //     include_once "controllers/product.php";
-        //     break;      
-        
-        case 'registerUser':
-            $user->registerUser();
-            break;
-        
-        case 'login':
-            $user->login();
-            break;
-        
-        case 'dangxuat':
-            $user->dangxuat();
-            break;
-
-        
-
-        // default:
-          
-        //     include_once "views/home.php";
-        //     break;
-    }
-
-  
-    // include_once "views/footer.php";
->>>>>>> 0b96c49fe8f16f6f6b8012c23f6f0dd035a1f97c
 ?>
