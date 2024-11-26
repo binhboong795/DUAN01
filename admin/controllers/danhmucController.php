09778855b0502fd98fcbaaf9b253c1034684d7cf
dang anh <dangthianh09022005@gmail.com>
2024-11-26 20:27:51 +0700

    crud danh mục

--- admin/controllers/danhmucController.php
+++ admin/controllers/danhmucController.php
@@ -8,7 +8,41 @@ class danhmucController
     }
     function danhmuc()
     {
-
-        require_once 'views/danhmuc.php';
+        $listDanhmuc = $this->danhmucModel->getAllDanhmuc();
+       
+        require_once 'views/danhmuc/danhmuc.php';
+    }
+    function deleteDanhmuc()
+    {
+        if ($_GET['id']) {
+            $id = $_GET['id'];
+            $id = $this->danhmucModel->deleteDanhmuc($id);
+        }
+        header('location:index.php?act=danhmuc');
+    }
+    function addDanhmuc()
+    {
+        if (isset($_POST['themmoi'])) {
+            $name = $_POST['name'];
+            $this->danhmucModel->addDanhmuc($name);
+            // Chuyển hướng sau khi thêm thành công
+            header('location:index.php?act=danhmuc');
+            exit; // Dừng script ngay sau khi chuyển hướng
+        }
+        require_once 'views/danhmuc/addDanhmuc.php';
+    }
+    function editDanhmuc()
+    {
+        $id = $_GET['id'] ?? null;
+        if ($id) {
+            $iddm = $this->danhmucModel->getIdDanhmuc($id);
+            if (isset($_POST['update_danhmuc'])) {
+                $name = $_POST['name'];
+                $this->danhmucModel->updateDanhmuc($id, $name);
+                header('location:index.php?act=danhmuc');
+                exit;
+            }
+        }
+        require_once 'views/danhmuc/editDanhmuc.php';
     }
 }
