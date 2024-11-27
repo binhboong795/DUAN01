e6fe72902ab6f54ad5be809bafeb3caa2c638fcb
duan1-thanhcong <nguyenthanhcongkg1996@gmail.com>
2024-11-27 11:16:21 +0700

    adf

--- admin/models/sanphamModel.php
+++ admin/models/sanphamModel.php
@@ -6,36 +6,4 @@ class sanphamModel
     {
         $this->conn = connectDB();
     }
-    function getAllSP()
-    {
-        $sql = "SELECT * FROM sanpham"; 
-        $stmt = $this->conn->prepare($sql);
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC); 
-    }
-    function insertProduct($id, $name, $price, $img, $mota, $luotxem, $iddm, $motachitiet, $soluong)
-    {
-        $sql = "INSERT INTO taikhoan (id, name, price, img, mota, luotxem, iddm, motachitiet, soluong) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-        $stmt = $this->conn->prepare($sql); // Chuẩn bị truy vấn với PDO
-        return $stmt->execute([$id, $name, $price, $img, $mota, $luotxem, $iddm, $motachitiet, $soluong]);
-    }
-    // function getIdUser($id)
-    // {
-    //     $sql = "SELECT * FROM taikhoan where id = ?"; 
-    //     $stmt = $this->conn->prepare($sql);
-    //     $stmt->execute([$id]);
-    //     return $stmt->fetch(); 
-    // }
-    // function editUser($id, $user, $pass, $email, $address, $tell, $role)
-    // {
-    //     $sql = "UPDATE taikhoan SET user = ?, pass = ?, email = ?, address = ?, tell = ?, $role = ? WHERE id = ?";
-    //     $stmt = $this->conn->prepare($sql);
-    //     return $stmt->execute([$user, $pass, $email, $address, $tell, $role, $id]);  
-    // }
-    function deleteUser($id)
-    {
-        $sql = "DELETE FROM `taikhoan` where id = ?";
-        $stmt = $this->conn->prepare($sql);
-        return $stmt->execute([$id]);
-    }
 }
