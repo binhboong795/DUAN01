3112d37fb1d5341076ebc76ab0b72d71677845a7
duan1-thanhcong <nguyenthanhcongkg1996@gmail.com>
2024-11-29 23:27:26 +0700

    cập nhật hủy đơn hàng

--- controllers/homeController.php
+++ controllers/homeController.php
@@ -626,17 +626,20 @@ class homeController
             $other_lido = $_POST['other_lido'];
             $ngayhuy = date('Y-m-d H:i:s');
             $iduser = $_SESSION['user']['id'];
-            $huydon = $this->homeModel->insertHuydon(null, $iduser, $ngayhuy, $lido, $other_lido);
 
-            $idbill = $_SESSION['id_bill'];
+            $iduser = $_SESSION['user']['id'];
+            $idbill = $this->homeModel->getIdBillByUser($iduser);
+            $bill_name = $this->homeModel->getIdNameStatus($iduser);
+
+            $huydon = $this->homeModel->insertHuydon(null, $bill_name, $iduser, $idbill, $ngayhuy, $lido, $other_lido);
+    
             $this->homeModel->deleteOrder($idbill);
-            // if (isset($_SESSION['idbill'])) {
-            //     unset($_SESSION['idbill']);
-            // }
+            $this->homeModel->deleteStatus($idbill);
+           
             echo "<script>alert('Bạn đã hủy thành công!')
                 window.location.href='index.php?act=chitietdonhang'
             </script>";
-        }
+        }   
         require_once 'views/huydonhang.php';
     }
 }
--- models/homeModel.php
+++ models/homeModel.php
@@ -358,10 +358,10 @@ class homeModel
     }
 
 
-    function insertHuydon($id, $id_user, $ngayhuy, $lido, $other_lido) {
-        $sql = "INSERT INTO huydonhang (id, id_user, ngayhuy, lido, other_lido) VALUES (?, ?, ?, ?, ?)";
+    function insertHuydon($id, $name, $id_user, $idbill, $ngayhuy, $lido, $other_lido) {
+        $sql = "INSERT INTO huydonhang (id, name, id_user, idbill, ngayhuy, lido, other_lido) VALUES (?, ?, ?, ?, ?, ?, ?)";
         $stmt = $this->conn->prepare($sql);
-        return $stmt->execute([$id, $id_user, $ngayhuy, $lido, $other_lido]);
+        return $stmt->execute([$id, $name, $id_user, $idbill, $ngayhuy, $lido, $other_lido]);
     }
 
     function deleteOrder($idbill) {
@@ -369,4 +369,22 @@ class homeModel
         $stmt = $this->conn->prepare($sql);
         return $stmt->execute([$idbill]);
     }
+    function deleteStatus($id_bill) {
+        $sql = "DELETE FROM trangthai WHERE id_bill = ?";
+        $stmt = $this->conn->prepare($sql);
+        return $stmt->execute([$id_bill]);
+    }
+    function getIdBillByUser($iduser) {
+        $sql = "SELECT idbill FROM orders WHERE iduser = ?";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([$iduser]);
+        return $stmt->fetchColumn(); 
+    }
+    function getIdNameStatus($id_user) {
+        $sql = "SELECT bill_name FROM trangthai WHERE id_user = ?";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([$id_user]);
+        return $stmt->fetchColumn(); 
+    }
+    
 }
